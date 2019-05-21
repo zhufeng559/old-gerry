48139eb27655bad6a26d9bb30bfb5fd4fdf18252
yigo <519043025@qq.com>
2019-05-21 19:14:13 +0800

    'hebing'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpService } from '../service/common/http.service';
 import { StorageService } from '../service/common/storage.service';
 import { Camera } from '@ionic-native/camera/ngx';
 import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,6 +24,7 @@ import { FileTransfer } from '@ionic-native/file-transfer/ngx';
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
     HttpService,
     StorageService,
     Camera,
